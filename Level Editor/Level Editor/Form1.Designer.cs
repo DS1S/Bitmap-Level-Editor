602d52001c8554d234766f8737481e2b66e848c0
Daniel Saunders <37748500+DS1S@users.noreply.github.com>
2021-07-29 15:56:59 -0400

    Initial Commit - repo creation

---
+++ Level Editor/Level Editor/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Level_Editor
+{
+    partial class EditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorComboBox = new System.Windows.Forms.ComboBox();
+            this.RGBLabel = new System.Windows.Forms.Label();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(778, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "MENU_BAR";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.createToolStripMenuItem,
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.createToolStripMenuItem.Text = "Create";
+            this.createToolStripMenuItem.Click += new System.EventHandler(this.createToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // colorComboBox
+            // 
+            this.colorComboBox.FormattingEnabled = true;
+            this.colorComboBox.Location = new System.Drawing.Point(489, 51);
+            this.colorComboBox.Name = "colorComboBox";
+            this.colorComboBox.Size = new System.Drawing.Size(152, 21);
+            this.colorComboBox.TabIndex = 1;
+            this.colorComboBox.SelectedIndexChanged += new System.EventHandler(this.colorComboBox_SelectedIndexChanged);
+            // 
+            // RGBLabel
+            // 
+            this.RGBLabel.AutoSize = true;
+            this.RGBLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RGBLabel.Location = new System.Drawing.Point(489, 89);
+            this.RGBLabel.Name = "RGBLabel";
+            this.RGBLabel.Size = new System.Drawing.Size(0, 20);
+            this.RGBLabel.TabIndex = 2;
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(493, 142);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(102, 26);
+            this.resetBtn.TabIndex = 3;
+            this.resetBtn.Text = "Reset Canvas";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // EditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 581);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.RGBLabel);
+            this.Controls.Add(this.colorComboBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "EditForm";
+            this.Text = "Level Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ComboBox colorComboBox;
+        private System.Windows.Forms.Label RGBLabel;
+        private System.Windows.Forms.Button resetBtn;
+    }
+}
+
---
+++ Level Editor/Level Editor/Framework/Tools/ColorSelection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Level_Editor.Framework.Tools
+{
+    public static class ColorSelection
+    {
+        public static Color selectedColor { get; set; }
+
+        public static Brush ConvertColorToBrush(Color color)
+        {           
+            PropertyInfo[] properties = typeof(Brushes).GetProperties();
+            for(int i = 0; i < properties.Length; i++)
+            {
+                
+                if (properties[i].Name.Equals(color.Name) && !color.IsEmpty)
+                {
+                    object tempBrush = properties[i].GetValue(properties);
+                    return (Brush)tempBrush;
+                }
+            }    
+            return null;
+        }
+
+        public static string[] GetListofColors()
+        {
+            string[] colors = null;
+            int counter = 0;
+            
+            PropertyInfo[] properties = typeof(Brushes).GetProperties();
+
+            colors = new string[properties.Length];
+
+            foreach(PropertyInfo p in properties)
+            {
+                colors[counter] = p.Name;
+                counter++;
+            }
+
+            return colors;
+        }
+
+        public static Color ConvertStringToColor(string colorName)
+        {
+            Color tempColor = Color.FromName(colorName);
+            return tempColor;
+        }
+    }
+}
